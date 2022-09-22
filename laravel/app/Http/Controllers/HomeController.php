e2b3ee57c5f6dc0d6082c56101f0166dd2b99cbb
Aiki Sano <osljoj@gmail.com>
2022-09-22 20:49:45 +0900

    コントローラ・ブレードname修正

--- laravel/app/Http/Controllers/HomeController.php
+++ laravel/app/Http/Controllers/HomeController.php
@@ -17,18 +17,5 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {   
-        $users = user::select('name')->get();
-        
-        return view('home', compact('users'));
-
-    }
-
 
 }
